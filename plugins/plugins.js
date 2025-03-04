1df3be46e7b8fb730f792e69fb25985d1ea122f8
Pr1nve <150605625+Pr1nve@users.noreply.github.com>
2025-03-04 12:33:11 +0100

    Update plugins.js

--- plugins/plugins.js
+++ plugins/plugins.js
@@ -91,3 +91,17 @@ bot(
     return await message.send(lang.plugins.remove.removed)
   }
 )
+const { setLydia, bot } = require('../lib/')
+
+bot({
+  pattern: 'lydia ?(.*)',
+  desc: 'To turn on/off chat bot for all contacts',
+  type: 'misc',
+},
+async (message, match) => {
+  if (!match) return await message.send('*Example : lydia on | off*')
+
+  const isOn = match.toLowerCase() === 'on'
+  await setLydia(message.jid, isOn, null, message.id)
+  await message.send(`_Lydia ${isOn ? 'Activated' : 'Deactivated'} for all contacts._`)
+})
